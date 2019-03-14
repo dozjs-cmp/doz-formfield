59ca084aefc80db6d3f822cf214d867d51b876e1
Pietro Lembo <p.lembo@xdevel.com>
2019-03-14 15:21:37 +0100

    BUILD

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,4 +1,4 @@
-// [DozFormfield]  Build version: 0.1.6  
+// [DozFormfield]  Build version: 0.1.4  
  (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory(require("doz"));
